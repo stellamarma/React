ba5189914d232ee1a4c23c0649ef386827bac86a
stellamarma <stellamarmat@gmail.com>
2025-01-29 18:34:26 +0200

    Create a function that return the year

--- 2.Components/src/components/header.jsx
+++ 2.Components/src/components/header.jsx
@@ -1,7 +1,14 @@
-const Header=()=>(
-        <div>
-            Header
-        </div>
-);
+const Header=()=>{
+    const getTheYear=() =>{
+        const newDate=new Date();
+        return newDate.getFullYear();
+    }
+
+    return(        <div>
+        The date is {getTheYear()}
+    </div>
+    )
+
+}
 
 export default Header;
