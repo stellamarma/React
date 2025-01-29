23d122d056c8206df39d942ec6254ef8aa43e4cf
stellamarma <stellamarmat@gmail.com>
2025-01-29 20:19:52 +0200

    Add some comments

--- 2.Components/src/main.jsx
+++ 2.Components/src/main.jsx
@@ -2,7 +2,7 @@
 import 'react';
 import ReactDOM from 'react-dom/client';
 import Header from './components/header';
-import NewsList from './components/news_list';
+//import NewsList from './components/news_list';
 
 const App = ()=> {
 
