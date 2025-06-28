72a55164eea702c4629b3b91c14c689a5b9b61a6
stellamarma <stellamarmat@gmail.com>
2025-06-28 11:45:08 +0300

    Add odjects to usestate

--- 2.Components/src/components/playground/state.jsx
+++ 2.Components/src/components/playground/state.jsx
@@ -2,27 +2,39 @@ import {useState} from 'react'
 
 const StatePlayground = () => {
    let initialCount = 0;
-    let [count,setCount] =useState(initialCount);
+    let [state,setState] =useState({
+        count:initialCount ,
+        user:"Stella"
+});
     
-
-    const addOne=()=>{
-        setCount(prevCount => prevCount + 1);
+    const new1 = ()=>{
+        setState(prevState=>{
+            return{
+                count: prevState.count +1,
+                user: "Maria"
+            }
+        }
+        )
     }
+    // const addOne=()=>{
+    //     setCount(prevCount => prevCount + 1);
+    // }
 
-    const decreaseOne=()=>{
-        setCount(prevCount => prevCount - 1);
-    }
+    // const decreaseOne=()=>{
+    //     setCount(prevCount => prevCount - 1);
+    // }
 
-    const resetCount = () =>{
-        setCount(initialCount);
-    }
-    console.log(count)
+    // const resetCount = () =>{
+    //     setCount(initialCount);
+    // }
+  
     return(
         <>
-            <h3>Count : {count}</h3>
-            <button onClick={addOne}>Add one</button>
-            <button onClick={decreaseOne}>Rest one</button>
-            <button onClick={resetCount}>Reset</button>
+        <h1>{state.user}</h1>
+            <h3>Count : {state.count}</h3>
+            <button onClick={new1}>Add one</button>
+            {/* <button onClick={decreaseOne}>Rest one</button>
+            <button onClick={resetCount}>Reset</button> */}
         </>
     )
 }
