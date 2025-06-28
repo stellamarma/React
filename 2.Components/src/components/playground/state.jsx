f05d189996f58f059559452255016c904db968ea
stellamarma <stellamarmat@gmail.com>
2025-06-28 10:39:18 +0300

    Add reset button

--- 2.Components/src/components/playground/state.jsx
+++ 2.Components/src/components/playground/state.jsx
@@ -1,8 +1,8 @@
 import {useState} from 'react'
 
 const StatePlayground = () => {
-   
-    let [count,setCount] =useState(0);
+   let initialCount = 0;
+    let [count,setCount] =useState(initialCount);
     
 
     const addOne=()=>{
@@ -13,12 +13,16 @@ const StatePlayground = () => {
         setCount(prevCount => prevCount - 1);
     }
 
+    const resetCount = () =>{
+        setCount(initialCount);
+    }
     console.log(count)
     return(
         <>
             <h3>Count : {count}</h3>
             <button onClick={addOne}>Add one</button>
             <button onClick={decreaseOne}>Rest one</button>
+            <button onClick={resetCount}>Reset</button>
         </>
     )
 }
