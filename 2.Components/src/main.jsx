7368c74046ac80e770b7b20d3cbf5acb76da9931
stellamarma <stellamarmat@gmail.com>
2025-06-29 08:54:35 +0300

    Add newsData to data.js

--- 2.Components/src/main.jsx
+++ 2.Components/src/main.jsx
@@ -2,16 +2,20 @@
 import 'react';
 import ReactDOM from 'react-dom/client';
 import Header from './components/Header/header';
-//import NewsList from './components/news_list';
+import NewsList from './components/news_list';
+import {newsData} from './components/utils/data'
 //import StatePlayground from './components/playground/state'
 
 const App = ()=> {
+
+  console.log(newsData)
 
     return (
         <>
           <Header />
           <div className='container'>
             {/* <StatePlayground /> */}
+            <NewsList/>
           </div>
         </>
     )
